d49c99613700dc70e5fd788269e06e9633a22cc8
celidur <66182659+celidur@users.noreply.github.com>
2023-03-15 05:24:59 +0000

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
+use std::env;
 
-fn main() {
-    let file = File::open("progmemTest.txt").expect("Unable to open file");
+fn compile(file_name: &str, debug: bool) -> Vec<u8> {
+    let file = File::open(file_name).expect("Unable to open file");
     let reader = BufReader::new(file);
     let set_instructions = [
         ("dbt", [0b00000001, 0]),
@@ -20,8 +21,8 @@ fn main() {
         ("fbc", [0b11000001, 0]),
         ("fin", [0b11111111, 0]),
     ];
-
-    let mut result = String::new();
+    // define result as a vector of u8
+    let mut result = Vec::new() as Vec<u8>;
     for line in reader.lines() {
         let line = line.unwrap().splitn(2, |c| c == '/' || c == '#' || c == '%').next().unwrap().to_string();
         if line.is_empty() {continue}
@@ -35,12 +36,22 @@ fn main() {
             let mut found_instruction = false;
             for (instruction, opcode) in &set_instructions {
                 if j.to_lowercase().starts_with(instruction) {
-                    result += &format!("{0:02x} ", opcode[0]);
+                    if debug {
+
+                    }
+                    result.push(opcode[0]);
                     if opcode[1] == 1 {
-                        result += &format!("{0:02x} ", j[instruction.len()..].trim().parse::<u8>().unwrap());
+                        if debug { 
+                            let number = j[instruction.len()..].trim().parse::<u8>().unwrap();
+                            println!("{} {} -> {:02x} {:02x}", instruction, number, opcode[0], number)
+                        }
+                        result.push(j[instruction.len()..].trim().parse::<u8>().unwrap());
                     }
                     else if !j[instruction.len()..].trim().is_empty() {
                         panic!("Unknown instruction: {}", j); // verify if the instruction has a parameter
+                    }
+                    else if debug {
+                        println!("{} -> {:02x}", instruction, opcode[0])
                     }
                     found_instruction = true;
                     break;
@@ -51,5 +62,23 @@ fn main() {
             }
         }
     }
-    println!("{}", result);
+    result
+}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let debug = args.len() > 1 && args[1] == "-v";
+    // recuperer le prochain argument
+    let mode = &args[1 + debug as usize];
+    if mode == "-o" {
+        let file_output = &args[2 + debug as usize];
+        let file_input = &args[3 + debug as usize];
+        // if file exists, delete it
+        let mut file = File::create(file_output).expect("Unable to create file");
+        let resultat = compile(file_input, debug);
+        // transform the vector of u8 to a string in hexadecimal
+        let res = resultat.iter().map(|x| format!("{:02x}", x)).collect::<Vec<String>>().join(" ");
+        file.write_all(&res.as_bytes()).expect("Unable to write data");
+        
+    }
+
 }
