5818d4b9199be026ad7a8866df35ca76ebb57a9e
celidur <66182659+celidur@users.noreply.github.com>
2025-05-26 12:28:28 -0400

    Fix decompiler mapping and optimize space handling

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,5 +1,4 @@
 use color_print::cprintln;
-use map_macro::map;
 use regex::Regex;
 use std::{
     fs::File,
@@ -8,33 +7,36 @@ use std::{
 
 use crate::errors::CompilerError;
 
+fn instruction_data(op: &str) -> Option<(u8, bool)> {
+    match op {
+        "dbt" => Some((0b0000_0001, false)),
+        "att" => Some((0b0000_0010, true)),
+        "dal" => Some((0b0100_0100, true)),
+        "det" => Some((0b0100_0101, false)),
+        "sgo" => Some((0b0100_1000, true)),
+        "sar" => Some((0b0000_1001, false)),
+        "mar" => Some((0b0110_0000, false)),
+        "mav" => Some((0b0110_0010, true)),
+        "mre" => Some((0b0110_0011, true)),
+        "trd" => Some((0b0110_0100, false)),
+        "trg" => Some((0b0110_0101, false)),
+        "dbc" => Some((0b1100_0000, true)),
+        "fbc" => Some((0b1100_0001, false)),
+        "fin" => Some((0b1111_1111, false)),
+        _ => None,
+    }
+}
+
 pub fn compile(file_name: String, silent: bool, optimize: bool) -> Result<Vec<u8>, CompilerError> {
     let file = File::open(file_name).expect("Unable to open file");
     let reader = BufReader::new(file);
-    let set_instructions = map! {
-    "dbt" => (0b00000001, false),
-    "att" => (0b00000010, true),
-    "dal" => (0b01000100, true),
-    "det" => (0b01000101, false),
-    "sgo" => (0b01001000, true),
-    "sar" => (0b00001001, false),
-    "mar" => (0b01100000, false),
-    "mav" => (0b01100010, true),
-    "mre" => (0b01100011, true),
-    "trd" => (0b01100100, false),
-    "trg" => (0b01100101, false),
-    "dbc" => (0b11000000, true),
-    "fbc" => (0b11000001, false),
-    "fin" => (0b11111111, false)};
     // define result as a vector of u8
     let mut result = vec![0, 0];
     let comments = Regex::new(r"(//|#|%).*").unwrap();
-    let remove_multiple_space = Regex::new("  ").unwrap();
-    let mut index_start = 0;
+    let remove_multiple_space = Regex::new(r"\s+").unwrap();
     let mut start = false;
     let mut end = false;
     let mut start_loop = false;
-    let mut remove_instruction = vec![];
     let mut can_print;
     let mut warnings = vec![];
     for (i, line) in reader.lines().enumerate() {
@@ -66,10 +68,9 @@ pub fn compile(file_name: String, silent: bool, optimize: bool) -> Result<Vec<u8
         }
 
         for instruction in instructions {
-            if instruction.is_empty() || (end && optimize) {
+            if instruction.is_empty() {
                 continue;
             }
-            can_print = !optimize || start;
             let instruction = remove_multiple_space.replace_all(instruction, " ");
             let mut iterator = instruction.split(' ');
             let operation = iterator.next().unwrap();
@@ -78,22 +79,31 @@ pub fn compile(file_name: String, silent: bool, optimize: bool) -> Result<Vec<u8
                 return Err(CompilerError::CaseError(i + 1, operation.to_string()));
             }
             let operation = operation.to_lowercase();
-            let Some(set_instruction) = set_instructions.get(operation.as_str()) else {
-                return Err(CompilerError::UnknownInstruction(i+1, instruction.to_string()))
+            let Some((opcode, needs_arg)) = instruction_data(&operation) else {
+                return Err(CompilerError::UnknownInstruction(
+                    i + 1,
+                    instruction.to_string(),
+                ));
             };
+
+            if optimize && (!start || end) && operation != "dbt" {
+                continue;
+            }
+
             if operation == "dbt" {
-                if start && optimize {
-                    remove_instruction.push(result.len());
-                    can_print = false;
+                if start {
+                    if optimize {
+                        continue;
+                    }
                 } else {
-                    index_start = result.len();
                     start = true;
-                    can_print = true;
                 }
             }
+
             if operation == "fin" && start {
                 end = true;
             }
+
             if operation == "dbc" {
                 if start_loop {
                     return Err(CompilerError::MultipleLoop(i + 1));
@@ -106,26 +116,32 @@ pub fn compile(file_name: String, silent: bool, optimize: bool) -> Result<Vec<u8
                 }
                 start_loop = false;
             }
-            if set_instruction.1 {
+
+            can_print = !optimize || start;
+
+            if needs_arg {
                 if argument.is_none() {
                     return Err(CompilerError::MissingOperand(i + 1, operation));
                 }
                 let Ok(argument) = argument.unwrap().parse::<u8>() else {
-                    return Err(CompilerError::InvalidOperand(i+1, operation, argument.unwrap().to_string()))
+                    return Err(CompilerError::InvalidOperand(
+                        i + 1,
+                        operation,
+                        argument.unwrap().to_string(),
+                    ));
                 };
-                result.push(set_instruction.0);
+                result.push(opcode);
                 result.push(argument);
                 if !silent && can_print {
                     cprintln!(
                         "<blue>{}</> <yellow>{}</>\t-> <blue>{:02x}</> <yellow>{:02x}</>",
                         operation,
                         argument,
-                        set_instruction.0,
+                        opcode,
                         argument
                     )
                 }
-            }
-            if !set_instruction.1 {
+            } else {
                 if argument.is_some() {
                     warnings.push(format!(
                         "Warning line {}: Instruction '{}' does not need an operand",
@@ -133,14 +149,10 @@ pub fn compile(file_name: String, silent: bool, optimize: bool) -> Result<Vec<u8
                         operation
                     ));
                 }
-                result.push(set_instruction.0);
+                result.push(opcode);
                 result.push(0);
                 if !silent && can_print {
-                    cprintln!(
-                        "<blue>{}</>\t-> <blue>{:02x}</> 00",
-                        operation,
-                        set_instruction.0
-                    )
+                    cprintln!("<blue>{}</>\t-> <blue>{:02x}</> 00", operation, opcode)
                 }
             }
         }
@@ -153,13 +165,6 @@ pub fn compile(file_name: String, silent: bool, optimize: bool) -> Result<Vec<u8
     }
     if start_loop {
         return Err(CompilerError::MissingLoopEnd);
-    }
-    if optimize {
-        for i in remove_instruction.iter().rev() {
-            result.remove(*i);
-            result.remove(*i);
-        }
-        result.drain(0..index_start - 2);
     }
     result[0] = (result.len() >> 8) as u8;
     result[1] = (result.len() & 0xff) as u8;
--- src/decompiler.rs
+++ src/decompiler.rs
@@ -1,26 +1,29 @@
 use crate::errors::DecompilerError;
 use color_print::cprintln;
-use map_macro::map;
 use std::fs;
 
+fn instruction_name(opcode: u8) -> Option<(&'static str, bool)> {
+    match opcode {
+        0b0000_0001 => Some(("dbt", false)),
+        0b0000_0010 => Some(("att", true)),
+        0b0100_0100 => Some(("dal", true)),
+        0b0100_0101 => Some(("det", false)),
+        0b0100_1000 => Some(("sgo", true)),
+        0b0000_1001 => Some(("sar", false)),
+        0b0110_0000 => Some(("mar", false)),
+        0b0110_0001 => Some(("mar", false)),
+        0b0110_0010 => Some(("mav", true)),
+        0b0110_0011 => Some(("mre", true)),
+        0b0110_0100 => Some(("trd", false)),
+        0b0110_0101 => Some(("trg", false)),
+        0b1100_0000 => Some(("dbc", true)),
+        0b1100_0001 => Some(("fbc", false)),
+        0b1111_1111 => Some(("fin", false)),
+        _ => None,
+    }
+}
+
 pub fn decompile(file_name: String, silent: bool) -> Result<String, DecompilerError> {
-    let set_instructions = map! {
-        0b00000001 => ("dbt", false),
-        0b00000010 => ("dbt", true),
-        0b01000100 => ("dal", true),
-        0b01000101 => ("det", false),
-        0b01001000 => ("sgo", true),
-        0b00001001 => ("sar", false),
-        0b01100000 => ("mar", false),
-        0b01100001 => ("mar", false),
-        0b01100010 => ("mav", true),
-        0b01100011 => ("mre", true),
-        0b01100100 => ("trd", false),
-        0b01100101 => ("trg", false),
-        0b11000000 => ("dbc", true),
-        0b11000001 => ("fbc", false),
-        0b11111111 => ("fin", false)
-    };
     let data = fs::read(file_name).expect("Unable to open file");
     if data.len() < 2 || data.len() % 2 == 1 {
         return Err(DecompilerError::FileFormat);
@@ -34,29 +37,25 @@ pub fn decompile(file_name: String, silent: bool) -> Result<String, DecompilerEr
         return Err(DecompilerError::FileFormat);
     }
     while let (Some(instruction), Some(argument)) = (data_iterator.next(), data_iterator.next()) {
-        let Some(set_instruction) = set_instructions.get(instruction) else {
-            return Err(DecompilerError::UnknownInstruction(*instruction))
+        let Some((name, needs_arg)) = instruction_name(*instruction) else {
+            return Err(DecompilerError::UnknownInstruction(*instruction));
         };
-        if set_instruction.1 {
+        if needs_arg {
             if !silent {
                 cprintln!(
                     "<blue>{:02x}</> <yellow>{:02x}</> -> <blue>{}</> <yellow>{}</>;",
                     instruction,
                     argument,
-                    set_instruction.0,
+                    name,
                     argument,
                 )
             }
-            result.push_str(&format!("{} {};\n", set_instruction.0, argument));
+            result.push_str(&format!("{} {};\n", name, argument));
         } else {
             if !silent {
-                cprintln!(
-                    "<blue>{:02x}</> 00 -> <blue>{}</>;",
-                    instruction,
-                    set_instruction.0,
-                )
+                cprintln!("<blue>{:02x}</> 00 -> <blue>{}</>;", instruction, name,)
             }
-            result.push_str(&format!("{};\n", set_instruction.0));
+            result.push_str(&format!("{};\n", name));
         }
     }
     Ok(result)
